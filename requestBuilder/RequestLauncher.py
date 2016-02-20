e2e36fd4370750fccba36289edb270651760815d
mdiaz <mdiazjusticia@gmail.com>
2016-02-20 11:28:59 +0100

    first request builder

---
+++ requestBuilder/RequestLauncher.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+key = 'AIzaSyCEKUxTbWeX0Ef78YoIYV81NY1jKfrn8ac'
+data = {
+  "request": {
+    "slice": [
+      {
+        "origin": "BCN",
+        "destination": "AMS",
+        "date": "2016-03-19"
+      }
+    ],
+    "passengers": {
+      "adultCount": 1
+    },
+    "solutions": 20,
+    "refundable": False
+  }
+}
+headers = {'content-type': 'application/json'}
+r = requests.post('https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + key,
+                 data = json.dumps(data),
+                headers = headers)
+
+response = r.json()
+print response
